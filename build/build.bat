68b7ab3b02da498bb241deecc3546016b159ea42
JohnBat26 <JohnBat26@03fd5646-7b3a-0410-8912-73af84f046af>
2008-10-07 19:27:28 +0000

    Изменение структуры

---
+++ build/build.bat
@@ -0,0 +1,20 @@
+@echo off
+
+rem ----- Verify JAVA_HOME environment variable -----
+
+if not "%JAVA_HOME%" == "" goto gotJavaHome
+echo *
+echo *    Error: The JAVA_HOME environment variable is not set.
+echo *    It must point at your JDK or JRE distribution, e.g.:
+echo *      JAVA_HOME=C:\j2re1.4.2_06
+goto exit
+
+:gotJavaHome
+
+set cp=..\..\lib\ant-launcher.jar
+set class=org.apache.tools.ant.launch.Launcher
+
+"%JAVA_HOME%\bin\java.exe" -showversion -Xmx512m -classpath %cp% -Dant.home=..\tools\lib %class% %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+
+:exit
